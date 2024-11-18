99ffab9130e7d4788e9c998ee34c8b540e259085
Gabo <smolana.gabriel@gmail.com>
2024-11-18 15:49:00 +0100

    feat: add sign-in and sign-up and update docker container

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return (
+    <div className="grid min-h-screen w-full place-items-center">
+      <SignIn />
+    </div>
+  );
+}
